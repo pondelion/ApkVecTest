aaa3bf7c2eef2ffa392230fc29f39abb34f38a56
pondelion <programming.deve@gmail.com>
2019-09-29 22:04:00 +0900

    update

---
+++ backend/apk_vec/crawler/apkmirror_crawler.py
@@ -0,0 +1,49 @@
+import numpu as np
+import requests
+from .base_crawler import BaseCrawler
+
+
+def RandomSamplingApkCrawler(BaseCrawler):
+
+    def __init__(self, sampling_num=100):
+        super(RandomSamplingApkCrawler, self).__init__()
+        self._MAX_FAIL_TOLERANCE = 1000
+        self._APK_URL_FMT =  'https://www.apkmirror.com/wp-content/themes/APKMirror/download.php?id={_id}'
+        self._SAMPLING_NUM = sampling_num
+        self._ZIP_MAGIC = 'PK\x03\x04'
+
+    def _crawl(self):
+        success_num = 0
+        fail_cnt = 0
+
+        while success_num < self._SAMPLING_NUM:
+            random_id = self._get_random_id()
+            r = requests.get(
+                self._APK_URL_FMT.format(_id=random_id),
+                allow_redirects=True
+            )
+
+            if not self._validate_apk(r.content[:4].decode('utf-8')):
+                fail_cnt += 1
+                if fail_cnt >= self._MAX_FAIL_TOLERANCE:
+                    break
+                continue
+
+            apk_filepath = f'/tmp/apkmirror_{random_id}.apk'
+            with open(apk_filepath, 'wb') as f:
+                f.write(r.content)
+
+            success_num += 1
+            yield apk_filepath
+
+            if success_num >= self._SAMPLING_NUM:
+                break
+
+    def _get_random_id(self):
+        return np.random.randint(1000, 5000000)
+
+    def _validate_apk(
+        self,
+        magic: str
+    ):
+        return magic == self._ZIP_MAGIC
--- backend/apk_vec/crawler/base_crawler.py
+++ backend/apk_vec/crawler/base_crawler.py
@@ -7,37 +7,35 @@ class BaseCrawler(metaclass=ABCMeta):
 
     def __init__(self):
         self._data = None
-        self._finish_callbacks = []
+        self._single_finish_callbacks = []
+        self._complete_callbacks = []
         self._fail_callbacks = []
 
     def _crawl_worker(
         self,
-        on_finish_callbacks: List[Callable[[Dict], None]]=list(),
+        on_single_finish_callbacks: List[Callable[[Dict], None]]=list(),
+        on_complete_callbacks: List[Callable[[Dict], None]]=list(),
         on_fail_callbacks: List[Callable[[str], None]]=list(),
     ):
         try:
-            self._data = self._crawl()
+            for filepath in self._crawl():
+                [callback(filepath) for callback in on_single_finish_callbacks]
         except Exception as e:
             [callback(e) for callback in on_fail_callbacks]
             return
 
-        [callback(self._data) for callback in on_finish_callbacks]
+        [callback(self._data) for callback in on_complete_callbacks]
 
     def crawl(
         self,
         async_: bool=False,
-        on_finish_callback: Callable[[Dict], None]=None,
+        on_single_finish_callback: Callable[[Dict], None]=None,
+        on_complete_callback: Callable[[Dict], None]=None,
         on_fail_callback: Callable[[str], None]=None,
     ):
-        if on_finish_callback is not None:
-            if not isinstance(on_finish_callback, list):
-                on_finish_callback = [on_finish_callback]
-            self._finish_callbacks += on_finish_callback
-
-        if on_fail_callback is not None:
-            if not isinstance(on_fail_callback, list):
-                on_fail_callback = [on_fail_callback]
-            self._fail_callbacks += on_fail_callback
+        self.add_single_finish_callback(on_single_finish_callback)
+        self.add_complete_callback(on_complete_callback)
+        self.add_fail_callback(on_fail_callback)
 
         if async_:
             thread = threading.Thread(
@@ -52,19 +50,37 @@ class BaseCrawler(metaclass=ABCMeta):
         else:
             self._data = self._crawl()
 
-    def add_finish_callback(
+    def add_single_finish_callback(
         self,
         callback: Callable[[Dict], None]
     ):
+        if callback is None:
+            return
+
         if not isinstance(callback, list):
             callback = [callback]
 
-        self._finish_callbacks += callback
+        self._single_finish_callbacks += callback
+
+    def add_complete_callback(
+        self,
+        callback: Callable[[Dict], None]
+    ):
+        if callback is None:
+            return
+
+        if not isinstance(callback, list):
+            callback = [callback]
+
+        self._complete_callbacks += callback
 
     def add_fail_callback(
         self,
         callback: Callable[[str], None]
     ):
+        if callback is None:
+            return
+
         if not isinstance(callback, list):
             callback = [callback]
 
@@ -72,4 +88,6 @@ class BaseCrawler(metaclass=ABCMeta):
 
     @abstractmethod
     def _crawl(self):
+        """Subclass must yealds local filepath of saved crawled data
+        """
         raise NotImplementedError
